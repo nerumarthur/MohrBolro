8adeff6f88f63b4b899981d9e84ba361fdf13cb8
Arthur Steinbach <stein.arth13@gmail.com>
2022-08-16 11:26:40 -0300

    #CHANGE: ADD SOUND

--- app/src/main/java/com/example/mohrbolro/TelaContador.java
+++ app/src/main/java/com/example/mohrbolro/TelaContador.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class TelaContador extends AppCompatActivity {
-
+    int contador = 0;
+    TextView contaNormal,contaPreoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_contador);
         getSupportActionBar().hide();
+        contaNormal = findViewById(R.id.campoAdq);
+        contaPreoc = findViewById(R.id.campoPrec);
     }
     public void Click(View view){
-        MediaPlayer snd = MediaPlayer.create(this, R.raw.SOUND);
+        if (contador < 4){
+            contador += 1;
+            contaNormal.setText(contador);
+        } else{
+          contador += 1;
+          contaPreoc.setText(contador);
+
+        }
+        MediaPlayer snd = MediaPlayer.create(this, R.raw.som);
         snd.start();
     }
 }
